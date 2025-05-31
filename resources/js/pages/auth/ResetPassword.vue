b197ff55afaf5ca1711e4963ce5db4b33707790b
wiley68 <ilko.iv@gmail.com>
2025-05-31 14:19:52 +0300

    create ResaetPassword page

--- resources/js/pages/auth/ResetPassword.vue
+++ resources/js/pages/auth/ResetPassword.vue
@@ -1,18 +1,11 @@
-<script setup lang="ts">
-import InputError from '@/components/InputError.vue';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import AuthLayout from '@/layouts/AuthLayout.vue';
+<script lang="ts" setup>
+import GuestLayout from '@/layouts/GuestLayout.vue';
 import { Head, useForm } from '@inertiajs/vue3';
-import { LoaderCircle } from 'lucide-vue-next';
 
-interface Props {
-    token: string;
+const props = defineProps<{
     email: string;
-}
-
-const props = defineProps<Props>();
+    token: string;
+}>();
 
 const form = useForm({
     token: props.token,
@@ -21,61 +14,52 @@ const form = useForm({
     password_confirmation: '',
 });
 
-const submit = () => {
+const onSubmit = () => {
     form.post(route('password.store'), {
-        onFinish: () => {
-            form.reset('password', 'password_confirmation');
-        },
+        onFinish: () => form.reset('password', 'password_confirmation'),
     });
 };
 </script>
 
 <template>
-    <AuthLayout title="Reset password" description="Please enter your new password below">
-        <Head title="Reset password" />
+    <GuestLayout>
+        <Head :title="__('Change password')"></Head>
 
-        <form @submit.prevent="submit">
-            <div class="grid gap-6">
-                <div class="grid gap-2">
-                    <Label for="email">Email</Label>
-                    <Input id="email" type="email" name="email" autocomplete="email" v-model="form.email" class="mt-1 block w-full" readonly />
-                    <InputError :message="form.errors.email" class="mt-2" />
-                </div>
-
-                <div class="grid gap-2">
-                    <Label for="password">Password</Label>
-                    <Input
-                        id="password"
-                        type="password"
-                        name="password"
-                        autocomplete="new-password"
-                        v-model="form.password"
-                        class="mt-1 block w-full"
-                        autofocus
-                        placeholder="Password"
-                    />
-                    <InputError :message="form.errors.password" />
-                </div>
-
-                <div class="grid gap-2">
-                    <Label for="password_confirmation"> Confirm Password </Label>
-                    <Input
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        autocomplete="new-password"
-                        v-model="form.password_confirmation"
-                        class="mt-1 block w-full"
-                        placeholder="Confirm password"
-                    />
-                    <InputError :message="form.errors.password_confirmation" />
-                </div>
-
-                <Button type="submit" class="mt-4 w-full" :disabled="form.processing">
-                    <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
-                    Reset password
-                </Button>
-            </div>
-        </form>
-    </AuthLayout>
+        <q-page class="column flex-center">
+            <q-card class="q-pa-md" style="width: 400px; max-width: 90vw">
+                <q-card-section class="q-ma-sm">
+                    <q-form @submit="onSubmit" class="q-gutter-md">
+                        <q-input
+                            v-model="form.email"
+                            :label="__('Email *')"
+                            :hint="__('Login email')"
+                            autocomplete="email"
+                            autofocus
+                            :error="form.hasErrors"
+                            :error-message="form.errors.email"
+                        />
+                        <q-input
+                            v-model="form.password"
+                            :label="__('Password *')"
+                            type="password"
+                            :hint="__('Login password')"
+                            :error="form.hasErrors"
+                            :error-message="form.errors.password"
+                        />
+                        <q-input
+                            v-model="form.password_confirmation"
+                            :label="__('Confirm password')"
+                            type="password"
+                            :hint="__('Confirm password')"
+                            :error="form.hasErrors"
+                            :error-message="form.errors.password_confirmation"
+                        />
+                        <div class="q-mt-lg row items-center justify-end">
+                            <q-btn label="Смяна на парола" color="primary" type="submit" class="full-width q-mt-md" />
+                        </div>
+                    </q-form>
+                </q-card-section>
+            </q-card>
+        </q-page>
+    </GuestLayout>
 </template>
