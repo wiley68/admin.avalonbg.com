84bf4308140087fc7bee18af4b40f21bea261433
wiley68 <ilko.iv@gmail.com>
2025-05-31 14:35:23 +0300

    change layouts path

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,49 +1,56 @@
 <script lang="ts" setup>
-import { Head, router } from '@inertiajs/vue3'
-import { usePermission } from '@/composables/permissions'
-import GuestLayout from '@/Layouts/GuestLayout.vue'
-import { useAppearance } from '@/composables/useAppearance'
-import LanguageSwitcher from '@/components/LanguageSwitcher.vue'
+import LanguageSwitcher from '@/components/LanguageSwitcher.vue';
+import { usePermission } from '@/composables/permissions';
+import { useAppearance } from '@/composables/useAppearance';
+import GuestLayout from '@/layouts/GuestLayout.vue';
+import { Head, router } from '@inertiajs/vue3';
 
 const props = defineProps<{
-    version: string
-    appName: string
-}>()
+    version: string;
+    appName: string;
+}>();
 
-const { hasUser, hasRole } = usePermission()
-const { theme } = useAppearance()
+const { hasUser, hasRole } = usePermission();
+const { theme } = useAppearance();
 </script>
 
 <template>
-
     <Head title="Начало"></Head>
 
     <GuestLayout>
-        <div class="column absolute-full flex flex-center" style="user-select: none">
-            <div class="row items-center justify-end absolute-top q-pa-md">
-                <q-btn-toggle v-model="theme" :options="[
-                    { label: __('Light'), value: 'light' },
-                    { label: __('Dark'), value: 'dark' },
-                    { label: __('Systematic'), value: 'system' }
-                ]" toggle-color="primary" push ripple />
+        <div class="column absolute-full flex-center flex" style="user-select: none">
+            <div class="row absolute-top q-pa-md items-center justify-end">
+                <q-btn-toggle
+                    v-model="theme"
+                    :options="[
+                        { label: __('Light'), value: 'light' },
+                        { label: __('Dark'), value: 'dark' },
+                        { label: __('Systematic'), value: 'system' },
+                    ]"
+                    toggle-color="primary"
+                    push
+                    ripple
+                />
 
                 <LanguageSwitcher class="q-ml-md" />
             </div>
-            <div class="column flex flex-center grow">
+            <div class="column flex-center flex grow">
                 <template v-if="hasUser()">
-                    <q-btn v-if="hasRole('admin')" @click="router.get(route('admin.index'))" class="text-primary"
-                        icon="dashboard" size="lg" label="Табло" />
-                    <q-btn v-else @click="router.get(route('dashboard'))" class="text-primary" icon="dashboard"
-                        size="lg" label="Табло" />
+                    <q-btn
+                        v-if="hasRole('admin')"
+                        @click="router.get(route('admin.index'))"
+                        class="text-primary"
+                        icon="dashboard"
+                        size="lg"
+                        label="Табло"
+                    />
+                    <q-btn v-else @click="router.get(route('dashboard'))" class="text-primary" icon="dashboard" size="lg" label="Табло" />
                 </template>
                 <template v-else>
-                    <q-btn @click="router.get(route('login'))" class="text-primary" icon="login" size="lg"
-                        label="Вход" />
+                    <q-btn @click="router.get(route('login'))" class="text-primary" icon="login" size="lg" label="Вход" />
                 </template>
                 <h1 class="text-weight-medium text-primary">{{ appName }}</h1>
-                <p class="text-subtitle1 text-weight-light">
-                    Laravel v.{{ props.version }} - {{ props.appName }}
-                </p>
+                <p class="text-subtitle1 text-weight-light">Laravel v.{{ props.version }} - {{ props.appName }}</p>
             </div>
         </div>
     </GuestLayout>
